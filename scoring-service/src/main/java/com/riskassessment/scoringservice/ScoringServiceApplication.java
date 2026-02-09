b4fb3ba092ce6e35e4ed7a6b77b62491d7453e9d
Aboussebaba Othman <aboussebaba.othman@gmail.com>
2026-02-09 14:38:45 +0100

    feat(scoring): configure EntityScan for JPA

--- scoring-service/src/main/java/com/riskassessment/scoringservice/ScoringServiceApplication.java
+++ scoring-service/src/main/java/com/riskassessment/scoringservice/ScoringServiceApplication.java
@@ -2,8 +2,10 @@ package com.riskassessment.scoringservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.riskassessment.scoring.entity")
 public class ScoringServiceApplication {
 
 	public static void main(String[] args) {
