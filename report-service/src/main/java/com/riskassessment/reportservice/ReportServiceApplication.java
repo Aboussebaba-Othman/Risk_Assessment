2a882d1fe0a37d84baa9316464d500891ef6cf50
Aboussebaba Othman <aboussebaba.othman@gmail.com>
2026-02-10 21:56:23 +0100

    feat(report): configure EntityScan for JPA

--- report-service/src/main/java/com/riskassessment/reportservice/ReportServiceApplication.java
+++ report-service/src/main/java/com/riskassessment/reportservice/ReportServiceApplication.java
@@ -2,8 +2,10 @@ package com.riskassessment.reportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.riskassessment.report.entity")
 public class ReportServiceApplication {
 
 	public static void main(String[] args) {
