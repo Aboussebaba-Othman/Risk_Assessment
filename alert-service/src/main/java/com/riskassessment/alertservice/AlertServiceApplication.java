a57460fc5b6d3a0556c924adc450f2573affe7b8
Aboussebaba Othman <aboussebaba.othman@gmail.com>
2026-02-10 22:06:28 +0100

    fix(alert): add spring-data-jpa and configure EntityScan

--- alert-service/src/main/java/com/riskassessment/alertservice/AlertServiceApplication.java
+++ alert-service/src/main/java/com/riskassessment/alertservice/AlertServiceApplication.java
@@ -2,8 +2,10 @@ package com.riskassessment.alertservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.riskassessment.alert.entity")
 public class AlertServiceApplication {
 
 	public static void main(String[] args) {
