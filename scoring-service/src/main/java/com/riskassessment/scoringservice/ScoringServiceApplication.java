a0af14da5cc9297622404a4340fa0d519fda4cfb
Aboussebaba Othman <aboussebaba.othman@gmail.com>
2026-02-13 15:06:21 +0100

    feat(scoring): enable OpenFeign for inter-service communication

--- scoring-service/src/main/java/com/riskassessment/scoringservice/ScoringServiceApplication.java
+++ scoring-service/src/main/java/com/riskassessment/scoringservice/ScoringServiceApplication.java
@@ -3,9 +3,11 @@ package com.riskassessment.scoringservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EntityScan("com.riskassessment.scoring.entity")
+@EnableFeignClients
 public class ScoringServiceApplication {
 
 	public static void main(String[] args) {
