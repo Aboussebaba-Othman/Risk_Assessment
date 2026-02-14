9ba55965bfc5f63a7c4b821d69d782e72e1045e4
Aboussebaba Othman <aboussebaba.othman@gmail.com>
2026-02-14 16:28:35 +0100

    feat(analysis): enable OpenFeign for inter-service communication

--- analysis-service/src/main/java/com/riskassessment/analysisservice/AnalysisServiceApplication.java
+++ analysis-service/src/main/java/com/riskassessment/analysisservice/AnalysisServiceApplication.java
@@ -3,9 +3,11 @@ package com.riskassessment.analysisservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EntityScan("com.riskassessment.analysis.entity")
+@EnableFeignClients
 public class AnalysisServiceApplication {
 
 	public static void main(String[] args) {
