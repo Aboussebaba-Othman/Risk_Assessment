4382ec1bd54db67d3e75a8176b9bb94a1975012b
Othman <othman@risk.com>
2026-03-25 18:43:50 +0100

    add MockBean JwtDecoder

--- auth-service/src/test/java/com/riskassessment/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/riskassessment/authservice/AuthServiceApplicationTests.java
@@ -2,7 +2,8 @@ package com.riskassessment.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
@@ -11,6 +12,9 @@ import org.springframework.test.context.ActiveProfiles;
 })
 @ActiveProfiles("test")
 class AuthServiceApplicationTests {
+
+	@MockBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
